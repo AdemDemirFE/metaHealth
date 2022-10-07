9e6588468b344d1212e3e5aaa7a6e151a9d0ec3d
Adem Demir <adem.demir.fe@gmail.com>
2022-10-08 01:37:22 +0300

    translete add

--- metaHealth/src/app/app.module.ts
+++ metaHealth/src/app/app.module.ts
@@ -6,11 +6,36 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HttpClientModule, HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateConfigService } from './translate-config.service';
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
+}
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (createTranslateLoader),
+        deps: [HttpClient],
+      }
+    }),
+  ],
+  providers: [
+    TranslateConfigService,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+
+  ],
+  bootstrap: [
+    AppComponent
+  ],
 })
 export class AppModule {}
